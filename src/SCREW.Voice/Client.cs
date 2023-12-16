91f4ed22566db99848d9cda846f017a77940d7f2
moonway <59399356+moonway-dev@users.noreply.github.com>
2023-12-16 05:38:08 +0300

    source code upload

---
+++ src/SCREW.Voice/Client.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using NAudio.Wave;
+
+namespace SCREW.Voice.Client
+{
+    public class ChatSettings
+    {
+        public string ipAddress { get; set; }
+        public int port { get; set; }
+        public string uid { get; set; }
+        public DeviceSettings deviceSettings { get; set; }
+    }
+
+    public class DeviceSettings
+    {
+        private int _bufferMilliseconds;
+
+        private int BufferMilliseconds
+        {
+            get { return _bufferMilliseconds; }
+            set
+            {
+                if (value > 512)
+                {
+                    _bufferMilliseconds = 512;
+                }
+                else if (value < 16)
+                {
+                    _bufferMilliseconds = 16;
+                }
+                else
+                {
+                    _bufferMilliseconds = value;
+                }
+            }
+        }
+
+        public int GetBufferMilliseconds()
+        {
+            return _bufferMilliseconds;
+        }
+
+        public void SetBufferMilliseconds(int value)
+        {
+            BufferMilliseconds = value;
+        }
+    }
+    public class Client
+    {
+        private UdpClient udpClient;
+        private WaveInEvent waveIn;
+        private WaveOut waveOut;
+        private BufferedWaveProvider bufferedWaveProvider;
+
+        public Client(ChatSettings chatSettings)
+        {
+            try
+            {
+                udpClient = new UdpClient();
+                ConnectToServer(chatSettings.ipAddress, chatSettings.port);
+
+                waveIn = new WaveInEvent();
+                waveIn.BufferMilliseconds = chatSettings.deviceSettings.GetBufferMilliseconds();
+                waveIn.WaveFormat = new WaveFormat(44100, 16, 1);
+                waveIn.DataAvailable += OnDataAvailable;
+
+                waveOut = new WaveOut();
+                bufferedWaveProvider = new BufferedWaveProvider(new WaveFormat(44100, 16, 1));
+                waveOut.Init(bufferedWaveProvider);
+                waveOut.Play();
+
+                byte[] usernameBytes = System.Text.Encoding.Default.GetBytes(chatSettings.uid);
+                udpClient.Send(usernameBytes, usernameBytes.Length);
+
+                waveIn.StartRecording();
+                StartListening();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[Error] SCREW Voice: {ex.Message}");
+            }
+        }
+
+        private void ConnectToServer(string ipAddress, int port)
+        {
+            try
+            {
+                IPAddress[] addresses = Dns.GetHostAddresses(ipAddress);
+                bool connected = false;
+
+                foreach (IPAddress address in addresses)
+                {
+                    try
+                    {
+                        udpClient.Connect(new IPEndPoint(address, port));
+                        connected = true;
+                        Console.WriteLine($"Connected to {address}");
+                        break;
+                    }
+                    catch
+                    {
+                        Console.WriteLine($"[Error] SCREW Voice: Failed to connect to {address}. Trying next address...");
+                    }
+                }
+
+                if (!connected)
+                {
+                    Console.WriteLine("[Error] SCREW Voice: Failed to connect to the server.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[Error] SCREW Voice: {ex.Message}");
+            }
+        }
+
+        private void OnDataAvailable(object sender, WaveInEventArgs e)
+        {
+            try
+            {
+                udpClient.Send(e.Buffer, e.BytesRecorded);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[Error] SCREW Voice: {ex.Message}");
+            }
+        }
+
+        private void StartListening()
+        {
+            try
+            {
+                udpClient.BeginReceive(OnVoiceReceived, null);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[Error] SCREW Voice: {ex.Message}");
+            }
+        }
+
+        private void OnVoiceReceived(IAsyncResult result)
+        {
+            try
+            {
+                IPEndPoint serverReceiveEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                byte[] receivedBytes = udpClient.EndReceive(result, ref serverReceiveEndPoint);
+
+                if (receivedBytes.Length > 0 && waveOut != null)
+                {
+                    bufferedWaveProvider.AddSamples(receivedBytes, 0, receivedBytes.Length);
+                }
+
+                udpClient.BeginReceive(OnVoiceReceived, null);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[Error] SCREW Voice: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ src/SCREW.Voice/Server.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCREW.Voice.Server
+{
+    public class ClientActivityTracker
+    {
+        private readonly Dictionary<IPEndPoint, DateTime> clientActivity;
+        private readonly TimeSpan timeoutDuration;
+
+        public ClientActivityTracker(TimeSpan timeout)
+        {
+            clientActivity = new Dictionary<IPEndPoint, DateTime>();
+            timeoutDuration = timeout;
+        }
+
+        public void UpdateActivity(IPEndPoint clientEndPoint)
+        {
+            clientActivity[clientEndPoint] = DateTime.Now;
+        }
+
+        public List<IPEndPoint> GetInactiveClients()
+        {
+            var inactiveClients = clientActivity.Where(pair => (DateTime.Now - pair.Value) > timeoutDuration)
+                                                .Select(pair => pair.Key)
+                                                .ToList();
+
+            foreach (var client in inactiveClients)
+            {
+                clientActivity.Remove(client);
+            }
+
+            return inactiveClients;
+        }
+    }
+
+    public class Server
+    {
+        private UdpClient udpServer;
+        private Dictionary<IPEndPoint, string> clientUids;
+        private readonly ClientActivityTracker activityTracker;
+
+        public event Action<string, IPEndPoint> ClientConnected;
+        public event Action<string, IPEndPoint> ClientDisconnected;
+
+        public Server(TimeSpan timeout, int port)
+        {
+            udpServer = new UdpClient(port);
+            clientUids = new Dictionary<IPEndPoint, string>();
+            activityTracker = new ClientActivityTracker(timeout);
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("[Server] SCREW Voice: Server started.");
+            Console.WriteLine("[Server] SCREW Voice: Waiting for clients...");
+            while (true)
+            {
+                try
+                {
+                    IPEndPoint senderEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                    byte[] receivedBytes = udpServer.Receive(ref senderEndPoint);
+
+                    if (!clientUids.ContainsKey(senderEndPoint))
+                    {
+                        string uid = System.Text.Encoding.Default.GetString(receivedBytes);
+                        clientUids.Add(senderEndPoint, uid);
+
+                        ClientConnected?.Invoke(uid, senderEndPoint);
+                    }
+
+                    activityTracker.UpdateActivity(senderEndPoint);
+
+                    foreach (var client in clientUids.Keys.ToList())
+                    {
+                        if (client.Equals(senderEndPoint)) continue;
+
+                        try
+                        {
+                            udpServer.Send(receivedBytes, receivedBytes.Length, client);
+                        }
+                        catch (SocketException)
+                        {
+                            ClientDisconnected?.Invoke(clientUids[client], client);
+                            clientUids.Remove(client);
+                        }
+                    }
+
+                    var inactiveClients = activityTracker.GetInactiveClients();
+                    foreach (var client in inactiveClients)
+                    {
+                        ClientDisconnected?.Invoke(clientUids[client], client);
+                        clientUids.Remove(client);
+                    }
+                }
+                catch { }
+            }
+        }
+    }
+}
